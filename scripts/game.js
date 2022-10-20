4193ce3bc0fdfe0caa05e6ca0ad3dc5b20fc8039
Charlie <charlottevfarmer@gmail.com>
2022-10-20 12:26:46 +0000

    add newGame & showScore tests

--- scripts/game.js
+++ scripts/game.js
@@ -11,13 +11,34 @@ let game = {
     // add values to empty choices array so test passes
     choices: ["button1", "button2", "button3", "button4"],
 }
-// test still won't pass because we haven't exported game 
-// and imported it into our test file
+
+// remember to export & import new function
+// 7.
+function newGame() {
+    game.score = 0;
+    // 8. 
+    // add matching code from tests in step 8
+    game.currentGame = [];
+    game.playerMoves = [];
+    // ^^ at this stage, newGame resets everything, but doesn't start a new game
+    // need to call showScore function - reset the score to zero on the DOM
+    // and addTurn function - add a turn to our currently empty sequence
+    // 11.
+    showScore();
+}
+
+// 10.
+// gets the element with the ID of score and sets its inner text to game.score
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+// 11. 
+// ^^ remember to add call to showScore at the end of the newGame function
+
+
+
 // at the end of game.js we'll add our module.exports = { }
 // curly braces because we'll be exporting more than  
 // one object and function from this file, so we need to put them in curly braces.
 // and in game.test.js we'll import it at the top
-
-
-
-module.exports = { game };
+module.exports = { game, newGame, showScore };
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -5,7 +5,7 @@
 // npm install --save-dev jest-environment-jsdom
 // not just npm install --save-dev
 
- const { game } = require("../game");
+const { game, newGame, showScore } = require("../game");
 
 // 1.
 // load the index.html file into Jests mock DOM
@@ -60,5 +60,48 @@ describe("game object contains correct key", () => {
     // receieved an empty array as we haven't added them to the game.js file
     test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
+
+// 6.
+    // newGame function should:
+    // reset the score to zero,  
+    // empty the computer sequence
+    // empty the player's moves array
+// new describe as separate from the game object
+describe("newGame works correctly", () => {
+    // use another beforeAll function, because we want to set up the game state 
+    // with some fake values to see if the new game function resets the score.
+    // it will fail at first as newGame is not defined
+    beforeAll(() => {
+        game.score = 42;
+        // 9.
+        // add some fake data to see if newGame clears it for tests in 8.
+        game.playerMoves = ["button1", "button2"];
+        game.currentGame = ["button1", "button2"];
+        // 10.
+        // add tests for showScore & addTurn functions
+        // set  the score on the DOM to fake number 
+        // so that we can see if it gets reset to zero by newGame. 
+        document.getElementById("score").innerText = "42"
+        newGame();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    // 8.
+    // test newGame resets currentGame & playerMoves arrays
+    // check if the length of the arrays after newGame runs is 0
+    // could use toEqual here, too
+    test("should reset currentGame array", () => {
+        expect(game.currentGame.length).toBe(0);
+    });
+    test("should reset playerMoves array", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+    // 10.
+    // this will fail at first, so we need to build the showScore function
+    test("should display 0 for the element with id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
     });
 });
